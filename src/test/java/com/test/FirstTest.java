73057c6f7aca28c830ce688412b42968d66ede99
pramaraju96 <pramaraju96@email.com>
2023-05-29 14:14:06 +0530

    TEST: new repo with java11 run

---
+++ src/test/java/com/test/FirstTest.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class FirstTest {
+
+	@Test
+	public void trueTest() {
+		assertTrue("Passed test", true);
+	}
+
+	@Test
+	public void falseTest() {
+		assertFalse("Failed test", true);
+	}
+
+	@Test
+	public void emptyTest() {
+		System.out.println("Empty test");
+	}
+
+	@Ignore
+	@Test
+	public void disabledTest() {
+		System.out.println("Ignored test");
+	}
+}
